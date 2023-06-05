8b6c304aaa3402c01af8854dcb18e217bd03705a
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-06-05 19:44:16 +0200

    added exception (#15)

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,6 +1,8 @@
 from src.video import Video
+from pprint import pprint
 
 if __name__ == '__main__':
     broken_video = Video('broken_video_id')
+    video = Video('AWX4JnAnjBE')
     assert broken_video.title is None
     assert broken_video.like_count is None
